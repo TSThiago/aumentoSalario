be077e2f8b62a3d2132e6977e8811efeadf39788
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-26 20:54:11 -0300

    Criação da variável 'porcentagem' dentro da função 'CalcularSalario'

--- aumentoSalario.js
+++ aumentoSalario.js
@@ -1,13 +1,3 @@
-// 1. Crie uma função que pergunte o nome e o salário de um
-// colaborador;
-// 2. Depois crie uma função que irá calcular o aumento de salário
-// conforme a seguinte tabela:
-// ATÉ R$1.500,00 : aumento de 20%
-// R$1.501,00 até R$2.000,00 : aumento de 15%
-// R$2.001,00 até R$3.000,00 : aumento de 10%
-// R$3.001,00 em diante : aumento de 5%
-// Essa função tem que receber dois parâmetros: o salário e a %
-// de aumento. E retornar o valor do salário já com o aumento.
 // 3. Depois de calcular o aumento você deve exibir: o nome do
 // colaborador, o salário, a % de aumento e o salário reajustado.
 // 4. Crie uma função que pergunte se o usuário deseja calcular
@@ -31,18 +21,23 @@ function PedirDados(){
 
 function CalcularSalario(num){
     if(num < 1501){
-        var salarioNovo = num * 1.20
+        var porcentagem = 1.20
+        var salarioNovo = num * porcentagem 
     }
     else if(num > 1500 && num < 2001){
-        salarioNovo = num * 1.15
+        porcentagem = 1.15
+        salarioNovo = num * porcentagem
     }
     else if(num > 2000 && num < 3001){
-        salarioNovo = num * 1.10
+        porcentagem = 1.10
+        salarioNovo = num * porcentagem 
     }
     else if(num > 3000){
-        salarioNovo = num * 1.10
+        porcentagem = 1.05
+        salarioNovo = num * porcentagem
     }
     console.log(salarioNovo)
+    return salarioNovo;
 }
 
 CalcularSalario(PedirDados())
