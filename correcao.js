036ecbdc67c70a6154b7d912d66eae59aaed1ad1
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-08-29 21:07:19 -0300

    Criação da função que calcula o aumento salarial.

--- correcao.js
+++ correcao.js
@@ -1,9 +1,24 @@
 var nome = 0
 var salario = 0
-
+var porcentagem = 0
 
 function PedirDados(){
     nome = prompt("Insira o seu nome")
     salario = parseFloat("Insira o seu salário atual")
 }
 
+function DefinirPorcentagem(){
+    if(salario < 1500){
+        porcentagem = 20
+    }else if(salario < 2000){
+        porgentagem = 15
+    }else if(salario < 3000){
+        porgentagem = 10    
+    }else{
+        porcentagem = 5
+    }
+}
+
+function CalcularSalario(salarioParametro, porcentagemParametro){
+    return salarioReajustado = salarioParametro + (salarioParametro * porcentagemParametro/100)
+}
